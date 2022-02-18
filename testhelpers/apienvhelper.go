f3f0afc9f353b5d7203353ea21fc89e625cc40da
Cameron Jones <camandrewjones@gmail.com>
2022-02-18 13:25:35 +0000

    Test handlers

---
+++ testhelpers/apienvhelper.go
@@ -0,0 +1,27 @@
+package testhelpers
+
+import (
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/wonesy/bookfahrt/api"
+	"github.com/wonesy/bookfahrt/ent/enttest"
+)
+
+func NewTestApiEnv(t *testing.T) *api.ApiEnv {
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+
+	return api.NewApiEnv(client)
+}
+
+func NewTestApp(t *testing.T) *fiber.App {
+	apiEnv := NewTestApiEnv(t)
+	app := fiber.New()
+	app.Route("/users", func(router fiber.Router) {
+		router.Post("", apiEnv.CreateUserHandler())
+		router.Get("/:username?", apiEnv.GetUserHandler())
+		router.Put("/:username", apiEnv.UpdateUserHandler())
+		router.Delete("/:username", apiEnv.DeleteUserHandler())
+	})
+	return app
+}
