d82c9d97cea82066e2ae098cb7ce995117da0190
Cameron Jones <camandrewjones@gmail.com>
2022-02-18 12:03:50 +0000

    Update user test

--- api/userhandlers_test.go
+++ api/userhandlers_test.go
@@ -3,6 +3,7 @@ package api_test
 import (
 	"testing"
 
+	"github.com/jinzhu/copier"
 	"github.com/stretchr/testify/assert"
 	"github.com/wonesy/bookfahrt/api"
 	"github.com/wonesy/bookfahrt/ent"
@@ -11,7 +12,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func TestCreateUser(t *testing.T) {
+func TestCreateDeleteUser(t *testing.T) {
 	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 
 	apiEnv := api.NewApiEnv(client)
@@ -36,4 +37,56 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, "test-password", createdUser.Password)
 
 	assert.EqualValues(t, createdUser.CreatedAt, gotUser.CreatedAt)
+
+	// clean up
+	num, err := apiEnv.DeleteUser("test-username")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, num)
+}
+
+func TestCreateUpdateDeleteUser(t *testing.T) {
+	client := enttest.Open(t, "sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+
+	apiEnv := api.NewApiEnv(client)
+
+	newUser := &ent.User{
+		Username:  "test-username",
+		FirstName: "test-firstname",
+		LastName:  "test-lastname",
+		Email:     "test-email@email.com",
+		Password:  "test-password",
+	}
+
+	createdUser, err1 := apiEnv.CreateUser(newUser)
+
+	assert.NoError(t, err1)
+	assert.Equal(t, "test-username", createdUser.Username)
+	assert.Equal(t, "test-firstname", createdUser.FirstName)
+	assert.Equal(t, "test-lastname", createdUser.LastName)
+	assert.Equal(t, "test-email@email.com", createdUser.Email)
+	assert.Equal(t, "test-password", createdUser.Password)
+
+	// update
+	update := &ent.User{}
+	copier.Copy(update, createdUser)
+	update.FirstName = "new first name"
+
+	numUpdated, err3 := apiEnv.UpdateUser(update)
+	gotUser, err2 := apiEnv.GetUserByUsername("test-username")
+
+	assert.NoError(t, err3)
+	assert.NoError(t, err2)
+	assert.Equal(t, 1, numUpdated)
+
+	assert.EqualValues(t, createdUser.CreatedAt, gotUser.CreatedAt)
+	assert.Equal(t, "test-username", gotUser.Username)
+	assert.Equal(t, "new first name", gotUser.FirstName)
+	assert.Equal(t, "test-lastname", gotUser.LastName)
+	assert.Equal(t, "test-email@email.com", gotUser.Email)
+	assert.Equal(t, "test-password", gotUser.Password)
+
+	// clean up
+	num, err := apiEnv.DeleteUser("test-username")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, num)
 }
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/go-openapi/inflect v0.19.0 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/hashicorp/hcl/v2 v2.10.0 // indirect
+	github.com/jinzhu/copier v0.3.5 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
