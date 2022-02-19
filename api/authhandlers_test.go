8bd4f83c91a7099b8b54e0f19828537f068dacd8
Cameron Jones <camandrewjones@gmail.com>
2022-02-19 10:06:38 +0200

    add logout test

--- api/authhandlers_test.go
+++ api/authhandlers_test.go
@@ -60,3 +60,64 @@ func TestLogin(t *testing.T) {
 
 	assert.NotEmpty(t, sessionID)
 }
+
+func TestLogout(t *testing.T) {
+	un := "test"
+	pw := "pass"
+
+	app, apienv := testhelpers.NewTestTools(t)
+
+	// create a new user with a new password
+	hashed, _ := auth.HashPassword(pw)
+	createdUser, err := apienv.CreateUser(&ent.User{
+		Username: un,
+		Password: hashed,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, un, createdUser.Username)
+	assert.True(t, auth.PasswordMatchesHash(pw, createdUser.Password))
+
+	// try logging in via client
+	var buf bytes.Buffer
+	e := json.NewEncoder(&buf).Encode(&auth.Credentials{
+		Username: un,
+		Password: pw,
+	})
+	assert.NoError(t, e)
+
+	req := httptest.NewRequest("POST", "/auth/login", &buf)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err2 := app.Test(req, -1)
+	assert.NoError(t, err2)
+	assert.Equal(t, 200, resp.StatusCode)
+
+	b, _ := io.ReadAll(resp.Body)
+	var user *ent.User
+	json.Unmarshal(b, &user)
+
+	assert.Equal(t, un, user.Username)
+
+	// verify that the user is saved in the session store
+	var sessionID string
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "session_id" {
+			sessionID = cookie.Value
+		}
+	}
+	assert.NotEmpty(t, sessionID)
+
+	req = httptest.NewRequest("GET", "/auth/logout", nil)
+	resp, err = app.Test(req, 1)
+	assert.NoError(t, err)
+
+	// make sure that the cookie has been deleted from the store
+	var removedSessionID string
+	for _, cookie := range resp.Cookies() {
+		if cookie.Name == "session_id" {
+			removedSessionID = cookie.Value
+		}
+	}
+	assert.Empty(t, removedSessionID)
+
+}
